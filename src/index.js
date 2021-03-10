bf9c1d75f248ae48880613f18ed62ff47c46ed10
julia2017-yg <julka.getman@gmail.com>
2021-03-10 21:18:39 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let rezult = [];
+  let split = String(n).split('');  
+  split.forEach(element => {
+    if (!isNaN(element)) {
+      rezult.push(element);
+    }
+  });
+
+  return rezult.reverse('').join('');
 }
